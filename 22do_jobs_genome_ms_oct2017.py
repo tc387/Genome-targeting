6327856e3b23ada33ee588cee932c6b053da44bb
James Daniel Farrell <j.d.farrell00@gmail.com>
2020-03-23 13:12:40 +0800

    reformatted existed Python scripts

--- 22do_jobs_genome_ms_oct2017.py
+++ 22do_jobs_genome_ms_oct2017.py
@@ -4,83 +4,83 @@
 # by tc387 starting Mar 4 for Rosalind's project on genome DNAcc interactions
 # this script is used for massive job submission for calculation of SANTA LUCIA free energies.
 
-import numpy as np
 import math
-import time
-import sys, getopt
-import os, subprocess
-from Bio.Seq import Seq # biopython
-import re
+import subprocess
+import sys
 
 #####  PARAMETERS #####
-if (len(sys.argv) < 7) :
-    print ('\n')
-    print ('ERROR!  missing arguments. Need 6 arguments:\n -Genome1_filename \n' \
-               ' -Genome2_filename \n -blob size \n -strand size \n -match strand length prescreening \n -Temperature \n')
-    print ('Exiting ......\n')
+if (len(sys.argv) < 7):
+    print('\n')
+    print('ERROR!  missing arguments. Need 6 arguments:\n -Genome1_filename \n' \
+          ' -Genome2_filename \n -blob size \n -strand size \n -match strand length prescreening \n -Temperature \n')
+    print('Exiting ......\n')
     quit()
 
-gen1file = str(sys.argv[1])   # 1st genome filename, blobs  (e.g. Escherichia_coli.gen)
+gen1file = str(sys.argv[1])  # 1st genome filename, blobs  (e.g. Escherichia_coli.gen)
 lblob = int(sys.argv[3])  # number of bp per blob
-gen2file = str(sys.argv[2])   # 2nd genome filename, strands  (e.g. rBacillus_subtilis.gen)
+gen2file = str(sys.argv[2])  # 2nd genome filename, strands  (e.g. rBacillus_subtilis.gen)
 lstrand = int(sys.argv[4])  # strand length - number of bp per strand
 matchslength = int(sys.argv[5])  # matching sequence length for prescreening
-Temperature = float(sys.argv[6])    # Temperature
+Temperature = float(sys.argv[6])  # Temperature
 
-blobsperbatch=100
+blobsperbatch = 100
 
 # read genome
 gen1str = "".join(line.rstrip("\n") for line in open(gen1file))  # full genome string
 nblobs = math.ceil(len(gen1str) / float(lblob))  ######################################################## HACK
-#main directory name
-gdirname = "nGen-" + gen1file[:-4] + "-"+gen2file[:-4] + "_lb" + str(lblob) + "_ls" + str(lstrand)  + \
-              "_lm" + str(matchslength) + "_T" + str(round(Temperature))
-scratchdirname = "/sharedscratch/tc387/cGIPSdat/" + gdirname 
+# main directory name
+gdirname = "nGen-" + gen1file[:-4] + "-" + gen2file[:-4] + "_lb" + str(lblob) + "_ls" + str(lstrand) + \
+           "_lm" + str(matchslength) + "_T" + str(round(Temperature))
+scratchdirname = "/sharedscratch/tc387/cGIPSdat/" + gdirname
 
 subprocess.run(["mkdir " + gdirname], shell=True)
 subprocess.run(["mkdir " + scratchdirname + "/"], shell=True)
-iblobstart=0 # starting blob number for the batch array
+iblobstart = 0  # starting blob number for the batch array
 jobrunargs = r"python3 ../../savealldg_cgips_oct2017.py ../../" + gen1file + r" ../../" + gen2file + " " + \
-                 str(lstrand) + " " + str(matchslength) + " " + str(Temperature) +  " " + str(lblob) + " "
+             str(lstrand) + " " + str(matchslength) + " " + str(Temperature) + " " + str(lblob) + " "
 
-
-#calculate the partition function (with all strands of genome 2) over all blobs in the genome 1
-for iiblob in range(nblobs) :
+# calculate the partition function (with all strands of genome 2) over all blobs in the genome 1
+for iiblob in range(nblobs):
     # job arguments
-#   arrayjobrunargs = r"../cgips.py~../" + gen1file + r"~../" + gen2file + "~" + \
- #                str(lstrand) + "~" + str(matchslength) + "~" + str(Temperature) +  "~" + str(lblob) + "~"    # make directory and submit job
-# make directory and submit job
-    dirname = "blobcgips_bg-" + gen1file[:-4] + "_sg-"+gen2file[:-4] + "_lb" + str(lblob) + "_ls" + str(lstrand)  + \
+    #   arrayjobrunargs = r"../cgips.py~../" + gen1file + r"~../" + gen2file + "~" + \
+    #                str(lstrand) + "~" + str(matchslength) + "~" + str(Temperature) +  "~" + str(lblob) + "~"    # make directory and submit job
+    # make directory and submit job
+    dirname = "blobcgips_bg-" + gen1file[:-4] + "_sg-" + gen2file[:-4] + "_lb" + str(lblob) + "_ls" + str(lstrand) + \
               "_lm" + str(matchslength) + "_ib" + str(iiblob)
     subprocess.run(["mkdir " + gdirname + "/" + dirname], shell=True)
-  #  print(jobrunargs)
-  #  subprocess.run([r"cat qsubs-master.sh | sed -e 's/JOBNAME11/cGIPS_ib" + str(iiblob) + r"/' | sed -e 's~JOBRUNARGS11~" + jobrunargs + r"~' > " + dirname + r"/qsub_local.sh"], shell=True)
-  #  subprocess.run(["cp " + "pfunc  cgips.py " + dirname + "/"], shell=True)
-    if ( iiblob % blobsperbatch == blobsperbatch-1) : # submit blobsperbatch blobs as a single slurm job
-        #subprocess.run(["sbatch --array=0-999 array-qsubs.sh " + str(iblobstart) + " " + str(arrayjobrunargs)], shell=True )
-        dirname0b = "blobcgips_bg-" + gen1file[:-4] + "_sg-"+gen2file[:-4] + "_lb" + str(lblob) + "_ls" + str(lstrand)  + \
-              "_lm" + str(matchslength) + "_ib" + str(iblobstart)
-       # batchjobrunargs = r"cd .. ; for ii in $(seq "+str(iblobstart)+" "+str(iblobstart+blobsperbatch-1) + r" ) ; do cd blobcg*_ib$ii ; " + jobrunargs + "$ii > screen.txt ; cd .. ; mv blobcg*_ib$ii " + scratchdirname + "/ ; done" 
-        batchjobrunargs = r"cd .. ; for ii in $(seq "+str(iblobstart)+" "+str(iblobstart+blobsperbatch-1) + r" ) ; do cd blobcg*_ib$ii ; " + jobrunargs + "$ii > screen.txt ; cd .. ; done" 
-        subprocess.run([r"cat qsubs-master.sh | sed -e 's/JOBNAME11/cGIPS_ib" + str(iblobstart) + "-" + str(iiblob) + r"/' | sed -e 's@JOBRUNARGS11@" + \
-                    batchjobrunargs + r"@' > " + gdirname + "/" + dirname0b  + r"/qsub_local.sh"], shell=True)
-        print( "Batch job iblob" + str(iblobstart))
-        slurmargs=["sbatch qsub_local.sh"]	
-        subprocess.Popen(slurmargs,cwd=gdirname+"/"+dirname0b ,shell=True)
-    
-        iblobstart=iiblob+1
+    #  print(jobrunargs)
+    #  subprocess.run([r"cat qsubs-master.sh | sed -e 's/JOBNAME11/cGIPS_ib" + str(iiblob) + r"/' | sed -e 's~JOBRUNARGS11~" + jobrunargs + r"~' > " + dirname + r"/qsub_local.sh"], shell=True)
+    #  subprocess.run(["cp " + "pfunc  cgips.py " + dirname + "/"], shell=True)
+    if (iiblob % blobsperbatch == blobsperbatch - 1):  # submit blobsperbatch blobs as a single slurm job
+        # subprocess.run(["sbatch --array=0-999 array-qsubs.sh " + str(iblobstart) + " " + str(arrayjobrunargs)], shell=True )
+        dirname0b = "blobcgips_bg-" + gen1file[:-4] + "_sg-" + gen2file[:-4] + "_lb" + str(lblob) + "_ls" + str(
+            lstrand) + \
+                    "_lm" + str(matchslength) + "_ib" + str(iblobstart)
+        # batchjobrunargs = r"cd .. ; for ii in $(seq "+str(iblobstart)+" "+str(iblobstart+blobsperbatch-1) + r" ) ; do cd blobcg*_ib$ii ; " + jobrunargs + "$ii > screen.txt ; cd .. ; mv blobcg*_ib$ii " + scratchdirname + "/ ; done"
+        batchjobrunargs = r"cd .. ; for ii in $(seq " + str(iblobstart) + " " + str(
+            iblobstart + blobsperbatch - 1) + r" ) ; do cd blobcg*_ib$ii ; " + jobrunargs + "$ii > screen.txt ; cd .. ; done"
+        subprocess.run([r"cat qsubs-master.sh | sed -e 's/JOBNAME11/cGIPS_ib" + str(iblobstart) + "-" + str(
+            iiblob) + r"/' | sed -e 's@JOBRUNARGS11@" + \
+                        batchjobrunargs + r"@' > " + gdirname + "/" + dirname0b + r"/qsub_local.sh"], shell=True)
+        print("Batch job iblob" + str(iblobstart))
+        slurmargs = ["sbatch qsub_local.sh"]
+        subprocess.Popen(slurmargs, cwd=gdirname + "/" + dirname0b, shell=True)
+
+        iblobstart = iiblob + 1
 # ru narray batch job
-#print("sbatch array-qsubs.sh " + str(nblobs) + " " + str(arrayjobrunargs))
-#jobname = "'GenB-" + gen1file[:-4] + "-" + gen2file[:-4] + "'"
-if (nblobs > iblobstart) : 
-    dirname0b = "blobcgips_bg-" + gen1file[:-4] + "_sg-"+gen2file[:-4] + "_lb" + str(lblob) + "_ls" + str(lstrand)  + "_lm" + str(matchslength) + "_ib" + str(iblobstart)
-    batchjobrunargs = r"cd .. ;for ii in $(seq "+str(iblobstart)+" "+str(nblobs-1) + r" ) ; do cd blobcg*_ib$ii ; " + jobrunargs + "$ii > screen.txt ; cd .. ; done" 
- 
-    subprocess.run([r"cat qsubs-master.sh | sed -e 's/JOBNAME11/cGIPS_ib" + str(iblobstart) + "-" + str(nblobs-1) + r"/' | sed -e 's@JOBRUNARGS11@" + \
-                    batchjobrunargs + r"@' > " + gdirname+"/"+dirname0b + r"/qsub_local.sh"], shell=True)
-    slurmargs=["sbatch qsub_local.sh"]	
-    subprocess.Popen(slurmargs,cwd=gdirname+"/"+dirname0b ,shell=True)
-    
- 
- #   subprocess.run(["sbatch --array=0-"+str(nblobs-iblobstart-1) + " array-qsubs.sh " + str(iblobstart) + " " + str(arrayjobrunargs)], shell=True )
-#subprocess.run(["sbatch --array=0-"+str(nblobs-1) + " array-qsubs.sh " + str(nblobs) + " " + str(arrayjobrunargs)], shell=True )
+# print("sbatch array-qsubs.sh " + str(nblobs) + " " + str(arrayjobrunargs))
+# jobname = "'GenB-" + gen1file[:-4] + "-" + gen2file[:-4] + "'"
+if (nblobs > iblobstart):
+    dirname0b = "blobcgips_bg-" + gen1file[:-4] + "_sg-" + gen2file[:-4] + "_lb" + str(lblob) + "_ls" + str(
+        lstrand) + "_lm" + str(matchslength) + "_ib" + str(iblobstart)
+    batchjobrunargs = r"cd .. ;for ii in $(seq " + str(iblobstart) + " " + str(
+        nblobs - 1) + r" ) ; do cd blobcg*_ib$ii ; " + jobrunargs + "$ii > screen.txt ; cd .. ; done"
+
+    subprocess.run([r"cat qsubs-master.sh | sed -e 's/JOBNAME11/cGIPS_ib" + str(iblobstart) + "-" + str(
+        nblobs - 1) + r"/' | sed -e 's@JOBRUNARGS11@" + \
+                    batchjobrunargs + r"@' > " + gdirname + "/" + dirname0b + r"/qsub_local.sh"], shell=True)
+    slurmargs = ["sbatch qsub_local.sh"]
+    subprocess.Popen(slurmargs, cwd=gdirname + "/" + dirname0b, shell=True)
+
+#   subprocess.run(["sbatch --array=0-"+str(nblobs-iblobstart-1) + " array-qsubs.sh " + str(iblobstart) + " " + str(arrayjobrunargs)], shell=True )
+# subprocess.run(["sbatch --array=0-"+str(nblobs-1) + " array-qsubs.sh " + str(nblobs) + " " + str(arrayjobrunargs)], shell=True )
--- ngsp_apr19.py
+++ ngsp_apr19.py
@@ -2,89 +2,89 @@
 # calculates the similarity between two genomes based on a matching length size
 # by tc387 starting Mar 22 for Rosalind's project on genome DNAcc interactions
 
-import numpy as np
 import math
-import time
-import sys, getopt
-import os, subprocess
-from Bio.Seq import Seq # biopython
 import re
+import subprocess
+import sys
+
+import numpy as np
+from Bio.Seq import Seq  # biopython
+
 
 # This Python script looks at two genomes and callculates the simlarity. The first genome is cut into 'blobs'.
 # The script searches for the number of complementary overlaps of length 'matchlength' in the second genome.
 def main():
-
-    if (len(sys.argv) < 5) :
-        print ('\n')
-        print ('ERROR!  missing arguments. Need 4 arguments:\n -Genome1_filename \n' + \
-               ' -Genome2_filename \n -blob size \n -match strand length \n')
-        print ('Exiting ......\n')
+    if (len(sys.argv) < 5):
+        print('\n')
+        print('ERROR!  missing arguments. Need 4 arguments:\n -Genome1_filename \n' + \
+              ' -Genome2_filename \n -blob size \n -match strand length \n')
+        print('Exiting ......\n')
         quit()
 
-    gen1file = str(sys.argv[1])   # 1st genome filename, blobs  (e.g. Escherichia_coli.gen)
+    gen1file = str(sys.argv[1])  # 1st genome filename, blobs  (e.g. Escherichia_coli.gen)
     lblob = int(sys.argv[3])  # number of bp per blob
-    gen2file = str(sys.argv[2])   # 2nd genome filename, strands  (e.g. rBacillus_subtilis.gen)
+    gen2file = str(sys.argv[2])  # 2nd genome filename, strands  (e.g. rBacillus_subtilis.gen)
     matchslength = int(sys.argv[4])  # matching sequence length for prescreening
 
-    if (matchslength > lblob) :
+    if (matchslength > lblob):
         raise ValueError("Error: matchslength cannot be larger than the blob size. ")
-        
-        
-    
-    #outfilename_dGbs = "outdG_bgen=" + gen1file + "_lb" + str(lblob) + "_ib" + str(iiblob) + "_sgen="+gen2file + "_ls" + str(lstrand) + "_lm" + str(matchslength) + ".dat"
-    outfilename = "out_sims_bg" + gen1file + "_sg="+gen2file + "_lb" + str(lblob) +  "_lm" + str(matchslength) +  ".dat"
 
-# END PARAMETERS
-# ============================================================================================== #
+    # outfilename_dGbs = "outdG_bgen=" + gen1file + "_lb" + str(lblob) + "_ib" + str(iiblob) + "_sgen="+gen2file + "_ls" + str(lstrand) + "_lm" + str(matchslength) + ".dat"
+    outfilename = "out_sims_bg" + gen1file + "_sg=" + gen2file + "_lb" + str(lblob) + "_lm" + str(matchslength) + ".dat"
+
+    # END PARAMETERS
+    # ============================================================================================== #
     # read genomes
-    gen1str = "".join(line.rstrip("\n") for line in open(gen1file)) #full genome string
-    gen1strc = "".join(line.rstrip("\n") for line in open("r"+gen1file))  # full genome string
-    gen2str = "".join(line.rstrip("\n") for line in open(gen2file)) #full genome string
-    gen2strc = "".join(line.rstrip("\n") for line in open("r"+gen2file))  # full genome string
-    gen1 = Genomefrac("gen1", len(gen1str),gen1str)   # convert to the Genomefrac object
-    gen2 = Genomefrac("gen2", len(gen2str),gen2str)
-    gen1c = Genomefrac("gen1",  len(gen1str), gen1strc)  # convert to the Genomefrac object
-    gen2c = Genomefrac("gen2",  len(gen2str), gen2strc)
+    gen1str = "".join(line.rstrip("\n") for line in open(gen1file))  # full genome string
+    gen1strc = "".join(line.rstrip("\n") for line in open("r" + gen1file))  # full genome string
+    gen2str = "".join(line.rstrip("\n") for line in open(gen2file))  # full genome string
+    gen2strc = "".join(line.rstrip("\n") for line in open("r" + gen2file))  # full genome string
+    gen1 = Genomefrac("gen1", len(gen1str), gen1str)  # convert to the Genomefrac object
+    gen2 = Genomefrac("gen2", len(gen2str), gen2str)
+    gen1c = Genomefrac("gen1", len(gen1str), gen1strc)  # convert to the Genomefrac object
+    gen2c = Genomefrac("gen2", len(gen2str), gen2strc)
 
     # cut genome
-    gen1blobs=cutGenome(gen1, lblob, cutpos="begin")   # cut from begining (full blob at the begining)
+    gen1blobs = cutGenome(gen1, lblob, cutpos="begin")  # cut from begining (full blob at the begining)
     gen1cblobs = cutGenome(gen1c, lblob, cutpos="begin")  # cut from begining (full blob at the begining)
     # gen2strands=cutGenome(gen2, lstrand, cutpos="end") # cut from end (full strand at the end)
     nblobs = math.ceil(len(gen1str) / float(lblob))
 
-    nmatches=np.zeros(2*nblobs)
-    similarity=np.zeros(2*nblobs)
+    nmatches = np.zeros(2 * nblobs)
+    similarity = np.zeros(2 * nblobs)
 
     outfile = open(outfilename, 'w')
 
     for iiblob in range(nblobs):
         print("Finnished.. ", iiblob / float(nblobs))
         # find matching occurrences in genome2
-        candLocList = getStrandOverlaps(pattern=str(Seq(str(gen1blobs[iiblob].sequence)).reverse_complement()), matchlength=matchslength, string=gen2str)
+        candLocList = getStrandOverlaps(pattern=str(Seq(str(gen1blobs[iiblob].sequence)).reverse_complement()),
+                                        matchlength=matchslength, string=gen2str)
         candLocList2 = getStrandOverlaps(pattern=str(Seq(str(gen1blobs[iiblob].sequence)).reverse_complement()),
-                                        matchlength=matchslength, string=gen2strc)
+                                         matchlength=matchslength, string=gen2strc)
 
         #    print (str(matchslength) + " " + str(Seq(str(gen1blobs[iiblob].sequence)).reverse_complement()) + "\n")
-    #    print(candLocList)
+        #    print(candLocList)
         nmatches[iiblob] = len(candLocList) + len(candLocList2)
-        outfile.write(str(iiblob) + " " + str(nmatches[iiblob].astype(np.float32) / (lblob-matchslength+1)) + "\n")
+        outfile.write(str(iiblob) + " " + str(nmatches[iiblob].astype(np.float32) / (lblob - matchslength + 1)) + "\n")
         outfile.flush()
     for iiblob in range(nblobs):
         print("Finnished.. ", iiblob / float(nblobs))
         # find matching occurrences in genome2
-        candLocList = getStrandOverlaps(pattern=str(Seq(str(gen1cblobs[iiblob].sequence)).reverse_complement()), matchlength=matchslength, string=gen2str)
-        candLocList2 = getStrandOverlaps(pattern=str(Seq(str(gen1cblobs[iiblob].sequence)).reverse_complement()), matchlength=matchslength, string=gen2strc)
+        candLocList = getStrandOverlaps(pattern=str(Seq(str(gen1cblobs[iiblob].sequence)).reverse_complement()),
+                                        matchlength=matchslength, string=gen2str)
+        candLocList2 = getStrandOverlaps(pattern=str(Seq(str(gen1cblobs[iiblob].sequence)).reverse_complement()),
+                                         matchlength=matchslength, string=gen2strc)
 
-    #    print (str(matchslength) + " " + str(Seq(str(gen1blobs[iiblob].sequence)).reverse_complement()) + "\n")
-    #    print(candLocList)
-        nmatches[iiblob+nblobs] = len(candLocList)+ len(candLocList2)
-        outfile.write(str(iiblob+nblobs) + " " + str(nmatches[iiblob+nblobs].astype(np.float32) / (lblob-matchslength+1)) + "\n")
+        #    print (str(matchslength) + " " + str(Seq(str(gen1blobs[iiblob].sequence)).reverse_complement()) + "\n")
+        #    print(candLocList)
+        nmatches[iiblob + nblobs] = len(candLocList) + len(candLocList2)
+        outfile.write(str(iiblob + nblobs) + " " + str(
+            nmatches[iiblob + nblobs].astype(np.float32) / (lblob - matchslength + 1)) + "\n")
         outfile.flush()
 
-
-    similarity = nmatches.astype(np.float32) / (lblob-matchslength+1)
-    outfile.write(r"#rel_overlaps " + str(sum(similarity[:])/(2*nblobs)) + "\n")
-
+    similarity = nmatches.astype(np.float32) / (lblob - matchslength + 1)
+    outfile.write(r"#rel_overlaps " + str(sum(similarity[:]) / (2 * nblobs)) + "\n")
 
     outfile.close()
 
@@ -102,14 +102,14 @@ def getStrandOverlaps(pattern, matchlength, string):
     if type(matchlength) is not int:
         raise ValueError('Error: matchlength in getStrandOverlaps must be integer')
 
-    lpattern=len(pattern) # pattern length
-    lstring=len(string)  # string length
-    if (matchlength > lpattern) or (matchlength > lstring) :
+    lpattern = len(pattern)  # pattern length
+    lstring = len(string)  # string length
+    if (matchlength > lpattern) or (matchlength > lstring):
         return None
-    matchlocations=[]
-    for ii in range(0,lpattern-matchlength+1) :
-        iipat = '(?=' + pattern[ii:matchlength+ii] + ')'
-        #[jj.start() for jj in re.finditer(iipat, string)]
+    matchlocations = []
+    for ii in range(0, lpattern - matchlength + 1):
+        iipat = '(?=' + pattern[ii:matchlength + ii] + ')'
+        # [jj.start() for jj in re.finditer(iipat, string)]
         matchlocations.extend([jj.start() for jj in re.finditer(iipat, string)])
     return matchlocations
 
@@ -117,99 +117,103 @@ def getStrandOverlaps(pattern, matchlength, string):
 ########  NuPACK Wrapper #########
 class Nuwrap(object):
     def __init__(self, nu_args=None, nstrands=None, strandlist=None, permutation=None):
-        self.nu_args=nu_args
+        self.nu_args = nu_args
         if strandlist is None:
             raise ValueError('Error: no strands supplied to NuPack wrapper Nuwrap')
         elif type(strandlist) is not list:
             raise ValueError('Error: strandlist must be a list! supplied to NuPack wrapper Nuwrap')
-        else :
-            self.strandlist=strandlist
+        else:
+            self.strandlist = strandlist
         if nstrands is None:
             self.nstrands = len(strandlist)
-        else :
-            self.nstrands=nstrands
-        if permutation is None :
+        else:
+            self.nstrands = nstrands
+        if permutation is None:
             # default to ordered permutation
-            self.permutation = " ".join([str(list(range(1,self.nstrands+1))[ii]) for ii in range (self.nstrands)])
-        elif type(permutation) is not str :
-            raise ValueError('Error: permutation argument of Nuwrap must be a string of integers, defaulting to a series')
-            self.permutation = " ".join([str(list(range(1,self.nstrands+1))[ii]) for ii in range (self.nstrands)])
-        else :
+            self.permutation = " ".join([str(list(range(1, self.nstrands + 1))[ii]) for ii in range(self.nstrands)])
+        elif type(permutation) is not str:
+            raise ValueError(
+                'Error: permutation argument of Nuwrap must be a string of integers, defaulting to a series')
+            self.permutation = " ".join([str(list(range(1, self.nstrands + 1))[ii]) for ii in range(self.nstrands)])
+        else:
             self.permutation = permutation
-        self.nu_stdout = ""   # stdout captured from NuPack
+        self.nu_stdout = ""  # stdout captured from NuPack
         self.runstatus = 0  # flag specifying whether the subprocess is running (0), has finnished (1) or returned an error (-1)
         self.delG = 0.0  # interaction free energy
 
     def run(self):
         # input string to NuPack stdin
         nu_instring = str(self.nstrands) + '\n' + "".join([self.strandlist[ii] + '\n' for ii in range(self.nstrands)]) + \
-            self.permutation + '\n'
-        nu_byteinstring=nu_instring.encode()  # encode in bytes
+                      self.permutation + '\n'
+        nu_byteinstring = nu_instring.encode()  # encode in bytes
         # run subprocess
         try:
-            nu_run = subprocess.run(self.nu_args, input=nu_byteinstring, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, \
-                                check=True, shell=True)
-        except subprocess.CalledProcessError as e :
+            nu_run = subprocess.run(self.nu_args, input=nu_byteinstring, stdout=subprocess.PIPE,
+                                    stderr=subprocess.STDOUT, \
+                                    check=True, shell=True)
+        except subprocess.CalledProcessError as e:
             self.runstatus = -1  # return error
-            self.nu_stdout = 'NuPack crashed... return code: '+ str(e.returncode) + '\nNuPack stdout: ' + e.stdout.decode()
-        else :
+            self.nu_stdout = 'NuPack crashed... return code: ' + str(
+                e.returncode) + '\nNuPack stdout: ' + e.stdout.decode()
+        else:
             # run finished well
             self.runstatus = 1
             self.nu_stdout = nu_run.stdout.decode()
 
             # get free energy from NuPack stdout
-            nu_stdout_lines = self.nu_stdout.split('\n') # split by lines
-            outlen = len(nu_stdout_lines) # # of stdout lines
-            self.delG = nu_stdout_lines[outlen-3]  # get free energy
+            nu_stdout_lines = self.nu_stdout.split('\n')  # split by lines
+            outlen = len(nu_stdout_lines)  # # of stdout lines
+            self.delG = nu_stdout_lines[outlen - 3]  # get free energy
+
 
 ########  GENOME CLASS  ##########
 class Genomefrac(object):
-    def __init__(self, genname="",size=0, sequence=None, genbpos=0, genblobindex=0):
-        self.genname=genname         # genome name
-        self.genbloc=genbpos         # genome location - starting base location in genome
+    def __init__(self, genname="", size=0, sequence=None, genbpos=0, genblobindex=0):
+        self.genname = genname  # genome name
+        self.genbloc = genbpos  # genome location - starting base location in genome
         self.genblobindex = genblobindex  # genome location - serial number of blob in genome
         self.size = size  # size in bases of the sequence
-        if sequence is None :
-            self.sequence='T'*size   # default to polyT
-        else :
-            self.sequence=sequence
-     #       self.size=len(sequence)
+        if sequence is None:
+            self.sequence = 'T' * size  # default to polyT
+        else:
+            self.sequence = sequence
 
+    #       self.size=len(sequence)
 
     def get_sequence(self, sizef, posindex):
-        if posindex+sizef <= self.size :
+        if posindex + sizef <= self.size:
             return self.sequence[posindex:posindex + sizef]
-        elif posindex < self.size :
-            return  self.sequence[posindex:self.size]
-        else :
+        elif posindex < self.size:
+            return self.sequence[posindex:self.size]
+        else:
             return None
+
 
 ####### CUT GENOME FUNCTION ######
 def cutGenome(Genome, fsize, cutpos):
     # cuts Genome into fragements of length size.
     ind = 0
     basepos = 0
-    fraglist = [] # first fragmement is here
-    if (cutpos=="begin"):
+    fraglist = []  # first fragmement is here
+    if (cutpos == "begin"):
         fraglist.append(Genomefrac(genname=Genome.genname + '_' + str(ind), size=fsize, \
-                               sequence=Genome.get_sequence(fsize, 0),
-                               genbpos=Genome.genbloc + basepos, genblobindex=ind))
+                                   sequence=Genome.get_sequence(fsize, 0),
+                                   genbpos=Genome.genbloc + basepos, genblobindex=ind))
         ind += 1
         basepos += fsize
-    elif (cutpos=="end") :
+    elif (cutpos == "end"):
         offset = Genome.size % fsize
-        if (offset == 0) :  # cannot have a zero length strand
+        if (offset == 0):  # cannot have a zero length strand
             offset = fsize
         fraglist.append(Genomefrac(genname=Genome.genname + '_' + str(ind), size=offset, \
                                    sequence=Genome.get_sequence(offset, 0),
                                    genbpos=Genome.genbloc + basepos, genblobindex=ind))
         ind += 1
         basepos += offset
-    else :
+    else:
         raise ValueError("Error with cut position in function cutGenome ")
 
-    while basepos < Genome.size :
-
+    while basepos < Genome.size:
         fraglist.append(Genomefrac(genname=Genome.genname + '_' + str(ind), size=fsize, \
                                    sequence=Genome.get_sequence(fsize, basepos),
                                    genbpos=Genome.genbloc + basepos, genblobindex=ind))
