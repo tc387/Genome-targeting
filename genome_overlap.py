a3308d6d70c03a517859f06e2881719c1d26a2b0
James Daniel Farrell <j.d.farrell00@gmail.com>
2020-03-25 17:12:21 +0800

    genome_overlap.py: minor modifications to the parser text

--- genome_overlap.py
+++ genome_overlap.py
@@ -5,11 +5,12 @@ def get_parser():
     parser = argparse.ArgumentParser(description="calculate the overlap score between a genome and all length l probes")
     parser.add_argument('length', type=int, help='probe length')
     parser.add_argument('threads', type=int, help='number of openmp threads to use', default=1)
-    parser.add_argument('-n', type=int, default=None, help='consider the n most common subsequences in the genomes')
+    parser.add_argument('-n', type=int, default=None, help=('only consider the n most common subsequences'
+                                                            ' in the genomes'))
     parser.add_argument('--compare', action='store_true', help='compare the scores of all pairs of genomes')
     parser.add_argument('--file', default=None, help='output file')
-    parser.add_argument('genomes', nargs="+", help=('one or more one-line files containing genomes (.gen)'
-                                                    ' or genbank records (.gb)'))
+    parser.add_argument('genomes', nargs="+", help=('one or more genbank records (.gb)'
+                                                    ' or one-line files containing genomes (.gen)'))
     return parser
 
 
@@ -23,13 +24,13 @@ def main():
     length = args.length
     threads = args.threads
     n = args.n
-    compare = args.compare
+    compare_flag = args.compare
     file = args.file
     score_function = power_four_score
 
     genomes = *map(Genome.read, gen_files),
     compute = Computer(genomes, length, score_function)
-    compute(threads, n, compare, file)
+    compute(threads, n, compare_flag, file)
 
 
 if __name__ == '__main__':
