e59e0b7a15229e621f60c5da1b57fc57e345e35e
James Daniel Farrell <j.d.farrell00@gmail.com>
2020-04-01 11:59:21 +0800

    genome_overlap: now uses exponential score, parser has rate option

--- genome_overlap.py
+++ genome_overlap.py
@@ -7,6 +7,7 @@ def get_parser():
     parser.add_argument('threads', type=int, help='number of openmp threads to use', default=1)
     parser.add_argument('-n', type=int, default=None, help=('only consider the n most common subsequences'
                                                             ' in the genomes'))
+    parser.add_argument('-r', '--rate', type=float, default=1, help='rate parameter in the exponential weight')
     parser.add_argument('--compare', action='store_true', help='compare the scores of all pairs of genomes')
     parser.add_argument('--file', default=None, help='output file')
     parser.add_argument('genomes', nargs="+", help=('one or more genbank records (.gb)'
@@ -15,7 +16,7 @@ def get_parser():
 
 
 def main():
-    from genometargeting import power_four_score, Genome, Computer
+    from genometargeting import exponential_score_factory, Genome, Computer
 
     parser = get_parser()
     args = parser.parse_args()
@@ -24,9 +25,10 @@ def main():
     length = args.length
     threads = args.threads
     n = args.n
+    rate = args.rate
     compare_flag = args.compare
     file = args.file
-    score_function = power_four_score
+    score_function = exponential_score_factory(rate, length)
 
     genomes = *map(Genome.read, gen_files),
     compute = Computer(genomes, length, score_function)
